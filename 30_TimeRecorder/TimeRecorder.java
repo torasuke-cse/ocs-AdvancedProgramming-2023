c50271d41761e9d24c45e7fd06a478aa6d247878
NOGI Masafumi <nogi.ocs@gmail.com>
2023-07-26 13:22:20 +0900

    TimeRecorder Ver.12

--- 30_TimeRecorder/TimeRecorder.java
+++ 30_TimeRecorder/TimeRecorder.java
@@ -1,9 +1,10 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Date;
 
@@ -141,7 +142,12 @@ public class TimeRecorder extends Object {
 
         try (
             BufferedReader aReader = new BufferedReader(
-                new FileReader(TimeRecorder.FILENAME)
+                new InputStreamReader(
+                    new FileInputStream(
+                        new File(TimeRecorder.FILENAME)
+                    ),
+                    TimeRecorder.CSV_ENCODING
+                )
             )
         ) {
             String line = null;
