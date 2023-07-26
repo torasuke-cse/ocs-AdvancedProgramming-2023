22040667f6d75822b05984b282da6f3a0e297ac5
NOGI Masafumi <nogi.ocs@gmail.com>
2023-07-26 12:28:30 +0900

    ClockMachine Ver.5

--- 20_ClockMachine/ClockMachine.java
+++ 20_ClockMachine/ClockMachine.java
@@ -12,24 +12,36 @@ public class ClockMachine extends Object {
             "October", "November", "December"
         };
 
+        // 曜日名（英名）
+        final String[] DAY_OF_WEEK_NAMES = {
+            "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"
+        };
+
         Calendar aCalendar = Calendar.getInstance();   // 現在日時の生成
 
         System.out.println(aCalendar.getClass().getName());   // インスタンスのクラス名を表示
 
-        int month = aCalendar.get(Calendar.MONTH);          // 月の取得
-        int day   = aCalendar.get(Calendar.DAY_OF_MONTH);   // 日の取得
+        int month     = aCalendar.get(Calendar.MONTH);          // 月の取得
+        int day       = aCalendar.get(Calendar.DAY_OF_MONTH);   // 日の取得
+        int dayOfWeek = aCalendar.get(Calendar.DAY_OF_WEEK);    // 曜日の取得
 
         int hour   = aCalendar.get(Calendar.HOUR_OF_DAY);   // 時の取得
         int minute = aCalendar.get(Calendar.MINUTE);        // 分の取得
 
-        // 現在日時の文字列を生成
-        String datetime = (month + 1) + "/" + day + " " + hour + ":" + minute;
+        // 曜日名の変換
+        String dayOfWeekName = DAY_OF_WEEK_NAMES[dayOfWeek - 1];
+
+        // 現在日付の文字列を生成
+        String dateString = (month + 1) + "/" + day + "(" + dayOfWeekName + ")";
+        
+        // 現在時刻の文字列を生成
+        String timeString = hour + ":" + minute;
 
         // 月名の変換
         String monthName = MONTH_NAMES[month];
         
         // 画面出力
-        System.out.println(datetime);
+        System.out.println(dateString + " " + timeString);
         System.out.println("It's " + monthName + "!");
         
     }
