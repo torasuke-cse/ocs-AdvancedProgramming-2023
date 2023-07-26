e57d63fb806da912d821200238640d8f05d46ad7
NOGI Masafumi <nogi.ocs@gmail.com>
2023-07-26 12:28:19 +0900

    ClockMachine Ver.4

--- 20_ClockMachine/ClockMachine.java
+++ 20_ClockMachine/ClockMachine.java
@@ -1,12 +1,37 @@
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 public class ClockMachine extends Object {
 
     public void perform() {
 
-        Calendar aCalendar = new GregorianCalendar();   // 現在日時の生成（西暦）
+        // 月名（英名）
+        final String[] MONTH_NAMES = {
+            "January", "February", "March",
+            "April",   "May",      "June",
+            "July",    "August",   "September",
+            "October", "November", "December"
+        };
 
+        Calendar aCalendar = Calendar.getInstance();   // 現在日時の生成
+
+        System.out.println(aCalendar.getClass().getName());   // インスタンスのクラス名を表示
+
+        int month = aCalendar.get(Calendar.MONTH);          // 月の取得
+        int day   = aCalendar.get(Calendar.DAY_OF_MONTH);   // 日の取得
+
+        int hour   = aCalendar.get(Calendar.HOUR_OF_DAY);   // 時の取得
+        int minute = aCalendar.get(Calendar.MINUTE);        // 分の取得
+
+        // 現在日時の文字列を生成
+        String datetime = (month + 1) + "/" + day + " " + hour + ":" + minute;
+
+        // 月名の変換
+        String monthName = MONTH_NAMES[month];
+        
+        // 画面出力
+        System.out.println(datetime);
+        System.out.println("It's " + monthName + "!");
+        
     }
     
 }
