74839c7497154544c56ac821991305aea0254201
NOGI Masafumi <nogi.ocs@gmail.com>
2023-07-26 12:29:08 +0900

    ClockMachine Ver.8

--- 20_ClockMachine/ClockMachine.java
+++ 20_ClockMachine/ClockMachine.java
@@ -34,12 +34,25 @@ public class ClockMachine extends Object {
     };
 
     /**
-     * 曜日名（英名のみ）
+     * 曜日名（英名/和名）
      */
     public static final String[][] DAY_NAMES = {
         {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"},
         {"日", "月", "火", "水", "木", "金", "土"}
     };
+
+    /**
+     * 現在日時のカレンダーを保持するフィールド。
+     */
+    private Calendar calendar = null;
+
+    /**
+     * 言語種別を保持するフィールド。
+     *
+     * 言語種別の定数群に示される値を保持する。
+     * 初期値は英語(英名)。
+     */
+    private int languageType = ClockMachine.IN_ENGLISH;
 
     /**
      * 時計の機能を再現する。
@@ -51,14 +64,16 @@ public class ClockMachine extends Object {
      */
     public void perform(int languageType) {
 
-        Calendar aCalendar = Calendar.getInstance();   // 現在日時の生成
+        // 各フィールドの設定
+        this.calendar = Calendar.getInstance();
+        this.languageType = languageType;
 
         // 現在日時の文字列を生成
-        String dateString = this.getDateString(aCalendar, languageType);
-        String timeString = this.getTimeString(aCalendar);
+        String dateString = this.getDateString();
+        String timeString = this.getTimeString();
 
         // 月名を取得
-        String monthName = this.getMonthName(aCalendar, languageType);
+        String monthName = this.getMonthName();
         
         // 画面出力
         System.out.println(dateString + " " + timeString);
@@ -71,19 +86,17 @@ public class ClockMachine extends Object {
      *
      * 指定された言語種別に応じた日付の文字列を生成して応答する。
      *
-     * @param aCalendar 現在日時のカレンダー
-     * @param languageType 言語種別
      * @return 日付の文字列
      */
-    private String getDateString(Calendar aCalendar, int languageType) {
+    private String getDateString() {
 
-        int monthIndex     = aCalendar.get(Calendar.MONTH);          // 月の添え字の取得：0〜11
-        int month          = monthIndex + 1;                         // 月の取得：1〜12
-        int day            = aCalendar.get(Calendar.DAY_OF_MONTH);   // 日の取得
-        int dayOfWeek      = aCalendar.get(Calendar.DAY_OF_WEEK);    // 曜日の取得：1〜7
-        int dayOfWeekIndex = dayOfWeek - 1;                          // 曜日の添え字の取得：0〜6
+        int monthIndex     = this.calendar.get(Calendar.MONTH);          // 月の添え字の取得：0〜11
+        int month          = monthIndex + 1;                             // 月の取得：1〜12
+        int day            = this.calendar.get(Calendar.DAY_OF_MONTH);   // 日の取得
+        int dayOfWeek      = this.calendar.get(Calendar.DAY_OF_WEEK);    // 曜日の取得：1〜7
+        int dayOfWeekIndex = dayOfWeek - 1;                              // 曜日の添え字の取得：0〜6
 
-        String dayName = ClockMachine.DAY_NAMES[languageType][dayOfWeekIndex];
+        String dayName = ClockMachine.DAY_NAMES[this.languageType][dayOfWeekIndex];
 
         String dateString = month + "/" + day + "(" + dayName + ")";
 
@@ -94,13 +107,12 @@ public class ClockMachine extends Object {
     /**
      * 時刻の文字列を生成して応答する。
      *
-     * @param aCalendar 現在日時のカレンダー
      * @return 時刻の文字列
      */
-    private String getTimeString(Calendar aCalendar) {
+    private String getTimeString() {
     
-        int hour   = aCalendar.get(Calendar.HOUR_OF_DAY);   // 時の取得
-        int minute = aCalendar.get(Calendar.MINUTE);        // 分の取得
+        int hour   = this.calendar.get(Calendar.HOUR_OF_DAY);   // 時の取得
+        int minute = this.calendar.get(Calendar.MINUTE);        // 分の取得
 
         String timeString = hour + ":" + minute;
 
@@ -113,15 +125,13 @@ public class ClockMachine extends Object {
      *
      * 指定された言語種別に応じた月名を応答する。
      *
-     * @param aCalendar 現在日時のカレンダー
-     * @param languageType 言語種別
      * @return 月名の文字列
      */
-    private String getMonthName(Calendar aCalendar, int languageType) {
+    private String getMonthName() {
 
-        int monthIndex = aCalendar.get(Calendar.MONTH);   // 月の添え字の取得：0〜11
+        int monthIndex = this.calendar.get(Calendar.MONTH);   // 月の添え字の取得：0〜11
 
-        String monthName = ClockMachine.MONTH_NAMES[languageType][monthIndex];
+        String monthName = ClockMachine.MONTH_NAMES[this.languageType][monthIndex];
 
         return monthName;
 
