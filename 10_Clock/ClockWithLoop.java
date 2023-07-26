af5245326c73da7b4b6142c17426cc99f6ce7518
NOGI Masafumi <nogi.ocs@gmail.com>
2023-07-26 12:25:25 +0900

    Clock Ver.4

--- 10_Clock/ClockWithLoop.java
+++ 10_Clock/ClockWithLoop.java
@@ -4,18 +4,20 @@ public class ClockWithLoop extends Object {
 
         System.out.println("This is a clock!!");
 
-        for (int hour = 1; hour <= 24; hour++) {
+        for (int hour = 1; hour <= 12; hour++) {
 
             System.out.println(hour);
 
-            if (hour == 12) {
-
-                System.out.println("It's lunch time!!");
-            
-            }
-            
         }
+        
+        System.out.println("It's lunch time!!");
 
+        for (int hour = 13; hour <= 24; hour++) {
+
+            System.out.println(hour);
+
+        }
+        
         System.out.println("Good night...");
 
     }
