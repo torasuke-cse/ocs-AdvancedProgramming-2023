810b0afa87303790b8919a3cabce7ba8df4b6200
NOGI Masafumi <nogi.ocs@gmail.com>
2023-07-26 12:27:49 +0900

    ClockMachine Ver.2

--- 20_ClockMachine/ClockMachine.java
+++ 20_ClockMachine/ClockMachine.java
@@ -1,23 +1,12 @@
+import java.util.Calendar;
 import java.util.Date;
 
 public class ClockMachine extends Object {
 
     public void perform() {
 
-        Date currentDate = new Date();   // 現在日時のオブジェクトを生成
+        Calendar aCalendar = new Calendar();   // エラー（インスタンスを生成できない）
 
-        int month = currentDate.getMonth();   // 月の取得
-        int day   = currentDate.getDate();    // 日の取得
-
-        int hour   = currentDate.getHours();     // 時の取得
-        int minute = currentDate.getMinutes();   // 分の取得
-
-        // 現在日時の文字列を生成
-        String datetime = (month + 1) + "/" + day + " " + hour + ":" + minute;
-
-        // 画面出力
-        System.out.println(datetime);
-        
     }
     
 }
